3b65e2ce67552683dc2483cfc32523855c6adfeb
Nishchal Gautam <cyberhck@users.noreply.github.com>
2026-09-12 07:59:15 +0400

    chore: sync files from template

--- release.config.js
+++ release.config.js
@@ -1,8 +1,6 @@
+const cfg = process.env.BRANCH == "main" ? [] : [{name: `${process.env.BRANCH}`, prerelease: "rc"}]
 module.exports = {
-    branches: [
-        {name: 'main'},
-        {name: `${process.env.BRANCH}`, prerelease: "rc"}
-    ],
+    branches: [{name: "main"}, ...cfg],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
